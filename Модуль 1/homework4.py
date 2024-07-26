bd8de6b5e64d30f43e13cfe740afff85cb05f47a
teexonr <vap42.t@mail.ru>
2024-07-26 20:40:58 +0700

    Create homework4.py

---
+++ Модуль 1/homework4.py
@@ -0,0 +1,10 @@
+# Организация программ и методы строк
+
+# my_string = input()
+my_string = 'My string'
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[1])

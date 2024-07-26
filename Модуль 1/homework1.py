7b55e5b27e98662568730cd87e8a7a671528e672
teexonr <vap42.t@mail.ru>
2024-07-26 20:40:50 +0700

    Create homework1.py

---
+++ Модуль 1/homework1.py
@@ -0,0 +1,9 @@
+# Переменные
+
+amount_hw = 12
+spend_time = 1.5
+course_name = 'Python'
+avg_time = spend_time / amount_hw
+
+print(f'Курс: {course_name}, всего задач: {amount_hw}, '
+        f'затрачено часов: {spend_time}, среднее время выполнения {avg_time} часа.')

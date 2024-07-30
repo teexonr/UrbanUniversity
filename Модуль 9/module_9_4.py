02fed44d080ac049e280d16f1c4162eecb7c4f54
teexonr <vap42.t@mail.ru>
2024-07-30 18:46:19 +0700

    Create module_9_4.py

---
+++ Модуль 9/module_9_4.py
@@ -0,0 +1,34 @@
+from random import choice
+
+
+# lambda
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+print(list(map(lambda x, y: x == y, first, second)))
+
+
+# function
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as file:
+            [print(data, file=file) for data in data_set]
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+# class
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())

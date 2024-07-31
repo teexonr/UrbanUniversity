bed40d904db38bf9997bbcba830d99b4237c4438
teexonr <vap42.t@mail.ru>
2024-08-01 02:10:37 +0700

    Update module_10_4.py

--- Модуль 10/module_10_4.py
+++ Модуль 10/module_10_4.py
@@ -1,6 +1,6 @@
 import queue
 import time
-from threading import Thread
+from threading import Thread, Lock
 
 
 class Table:
@@ -10,15 +10,18 @@ class Table:
 
 
 class Customer(Thread):
-    def __init__(self, number, table):
+    def __init__(self, number, table, cafe):
         super().__init__()
         self.number = number
         self.table = table
+        self.cafe = cafe
 
     def run(self):
         time.sleep(5)
-        print(f'Посетитель номер {self.number} покушал за столиком и ушёл.')
-        self.table.is_busy = False
+        with self.cafe.lock:
+            print(f'Посетитель номер {self.number} покушал за столиком и ушёл.')
+            self.table.is_busy = False
+            self.cafe.check_queue()
 
 
 class Cafe:
@@ -26,6 +29,7 @@ class Cafe:
         self.queue = queue.Queue()
         self.tables = tables
         self.customers = []
+        self.lock = Lock()
 
     def customer_arrival(self):
         for people in range(1, 21):
@@ -34,31 +38,36 @@ class Cafe:
             self.serve_customer(people)
 
     def serve_customer(self, people):
-        for table in self.tables:
-            if not table.is_busy:
+        with self.lock:
+            for table in self.tables:
+                if not table.is_busy:
+                    self.seat_customer(people, table)
+                    break
+            else:
                 self.queue.put(people)
-                customer = Customer(self.queue.get(), table)
-                table.is_busy = True
-                print(f'Посетитель номер {customer.number} сел за стол {table.number}.')
-                customer.start()
-                self.customers.append(customer)
-                break
-        else:
-            self.queue.put(people)
-            print(f'Посетитель номер {people} ожидает свободный стол.')
+                print(f'Посетитель номер {people} ожидает свободный стол.')
+
+    def seat_customer(self, people, table):
+        customer = Customer(people, table, self)
+        self.customers.append(customer)
+        print(f'Посетитель номер {people} сел за стол {table.number}.')
+        table.is_busy = True
+        customer.start()
+
+    def check_queue(self):
+        if not self.queue.empty():
+            next_customer = self.queue.get()
+            for table in self.tables:
+                if not table.is_busy:
+                    self.seat_customer(next_customer, table)
+                    break
 
 
-# Создаем столики в кафе
 tables_list = [Table(1), Table(2), Table(3)]
-
-# Инициализируем кафе
 cafe = Cafe(tables_list)
 
-# Запускаем поток для прибытия посетителей
 customer_arrival_thread = Thread(target=cafe.customer_arrival)
 customer_arrival_thread.start()
-
-# Ожидаем завершения работы прибытия посетителей
 customer_arrival_thread.join()
 
 for customer_ in cafe.customers:

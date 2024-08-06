70ab68b5585c48bbcba2b2fb2679bf2dfd82b427
teexonr <140201304+teexonr@users.noreply.github.com>
2024-08-06 22:17:15 +0700

    Update user.py

--- Модуль 17/module_17_2/app/models/user.py
+++ Модуль 17/module_17_2/app/models/user.py
@@ -7,12 +7,12 @@ class User(Base):
     __tablename__ = 'users'
     __table_args__ = {'extend_existing': True}
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, index=True)
     username = Column(String)
     firstname = Column(String)
     lastname = Column(String)
     age = Column(Integer)
-    slug = Column(String)
+    slug = Column(String, unique=True, index=True)
 
     tasks = relationship('Task', back_populates='user')
 

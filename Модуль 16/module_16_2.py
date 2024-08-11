ed317376113a0bd1b920508f883434abc61b5ef8
teexonr <vap42.t@mail.ru>
2024-08-11 18:51:26 +0700

    Update module_16_2.py

--- Модуль 16/module_16_2.py
+++ Модуль 16/module_16_2.py
@@ -5,11 +5,12 @@ app = FastAPI()
 
 
 @app.get('/user/{user_id}')
-async def user_page(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID')]) -> str:
+async def user_page(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example=1)]) -> str:
     return f'Вы вошли как пользователь № {user_id}'
 
 
 @app.get('/user/{username}/{age}')
-async def user_info(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')],
-                    age: Annotated[int, Path(ge=18, le=120, description='Enter age')]) -> str:
+async def user_info(username: Annotated[str, Path(min_length=5, max_length=20,
+                                                  description='Enter username', example='UrbanUser')],
+                    age: Annotated[int, Path(ge=18, le=120, description='Enter age', example=24)]) -> str:
     return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
